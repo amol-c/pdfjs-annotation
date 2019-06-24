4efb48d9e196974b09f7e3b3390c42d8d91c7742
amol-c <amol@teacherspayteachers.com>
2019-06-24 12:56:19 -0400

    Working text

--- pdf-image/pdf-image-poc/src/App.js
+++ pdf-image/pdf-image-poc/src/App.js
@@ -1,4 +1,4 @@
-import React, {useReducer, useEffect} from 'react';
+import React, {useReducer, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Toolbar,TodosDispatch} from "./Toolbar/Toolbar"
@@ -6,34 +6,34 @@ import { TextBoxAnnotation, texboxAnnotationReducer } from "./Annotations/Textbo
 import {fabric} from "fabric"
 import $ from "jquery"
 
-let canvas: fabric.Canvas
+function App() {
+  const initialState = {}
+  const [_, dispatch] = useReducer(texboxAnnotationReducer, initialState);
+  let [canvas, setCanvas] = useState(null);
 
-$( document ).ready(function() {
+  useEffect(() => {
     if(!canvas) {
-      canvas = new fabric.Canvas('the-canvas', {
+      const fabricCanvas = new fabric.Canvas('the-canvas', {
         isDrawingMode: true
       });
-      canvas.backgroundColor = "white"
-      canvas.freeDrawingBrush.color = "red";
-      canvas.freeDrawingBrush.width = parseInt(2, 10) || 1;
-      canvas.freeDrawingBrush.shadow = new fabric.Shadow({
+      fabricCanvas.backgroundColor = "white"
+      fabricCanvas.freeDrawingBrush.color = "red";
+      fabricCanvas.freeDrawingBrush.width = parseInt(2, 10) || 1;
+      fabricCanvas.freeDrawingBrush.shadow = new fabric.Shadow({
         blur: parseInt(2, 10) || 0,
         offsetX: 0,
         offsetY: 0,
         affectStroke: true,
         color: "red",
       });
-      canvas.renderAll()  
-    } 
-});
-
-function App() {
-  const initialState = {}
-  const [_, dispatch] = useReducer(texboxAnnotationReducer, initialState);
+      fabricCanvas.renderAll()
+      setCanvas(fabricCanvas) 
+    }
+  });
 
   return (
     <div className="App">
-            <Toolbar annotationDispatch={dispatch}/>
+      <Toolbar annotationDispatch={dispatch}/>
       <header className="App-header">
         <canvas className="A4 page" id="the-canvas" width="480" height="600"></canvas>
       </header>
